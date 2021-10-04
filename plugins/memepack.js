6f6ecafd73013782ea57ea74a1f20b5280c22071
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-10-04 11:25:06 +0530

    Update memepack.js

--- plugins/memepack.js
+++ plugins/memepack.js
@@ -10,15 +10,15 @@ Asena.addCommand({pattern: 'mmpack', fromMe: true, desc: 'alexa meme pack'}, (as
 
 	        var r_text = new Array ();
 
-        r_text[1] = "https://telegra.ph/file/0d5425a8dd1b5ad3e3d81.jpg";
-        r_text[2] ="https://telegra.ph/file/4efb53aba19b0f1ccad12.jpg";
-        r_text[3] ="https://telegra.ph/file/7e18f85e60d0fe643c5d4.jpg";
-        r_text[4] ="https://telegra.ph/file/7b068a15a9b0adb97064d.jpg";
-        r_text[5] ="https://telegra.ph/file/b92174516f031df6ebd26.jpg";
+        r_text[1] = "https://telegra.ph/file/dedafa96811fddf072acc.jpg";
+        r_text[2] ="https://telegra.ph/file/7f3e1f377f1bdff4f2b8f.jpg";
+        r_text[3] ="https://telegra.ph/file/427f51e729760c151212e.jpg";
+        r_text[4] ="https://telegra.ph/file/bc1100723e88671d00484.jpg";
+        r_text[5] ="https://telegra.ph/file/b20bc54c41f6f8d85eeee.jpg";
 
 var i = Math.floor(06*Math.random())
 var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '   *╔═════◉ɴᴇᴏᴛʀᴏ◉═════╗*\n           🎭ɴᴇᴏᴛʀᴏ-𝙭🪐\n*❖═මීම් ලැයිස්තුව═❖*\n\n*💠.yasai       ❴your text❵*\n*💠.uddika    ❴your text❵*\n*💠.aah          ❴your text❵*\n*💠.sir            ❴your text❵*\n*💠.my3         ❴your text❵*\n*💠.fuck         ❴your text❵*\n*💠.hii             ❴your text❵*\n*💠.asai          ❴your text❵*\n*💠.hapoi       ❴your text❵*\n*💠.anura       ❴your text❵*\n*💠.gemba       ❴your text❵*\n*💠.wow       ❴your text❵*\n*💠.hapo       ❴your text❵*\n*💠.babah       ❴your text❵*\n*💠.macn       ❴your text❵*\n*💠.nah       ❴your text❵*\n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Template Credit: එ.මි.ස ¡v* '})
+await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '*◄━━━━━━━⦁⦁◯⦁⦁━━━━━━━━►*\n*QUEEN ALEXA MEME PACK*\n        \n*❖═මීම් ලැයිස්තුව═❖*\n\n*💠.yasai       ❴your text❵*\n*💠.uddika    ❴your text❵*\n*💠.aah          ❴your text❵*\n*💠.sir            ❴your text❵*\n*💠.my3         ❴your text❵*\n*💠.fuck         ❴your text❵*\n*💠.hii             ❴your text❵*\n*💠.asai          ❴your text❵*\n*💠.hapoi       ❴your text❵*\n*💠.anura       ❴your text❵*\n*💠.gemba       ❴your text❵*\n*💠.wow       ❴your text❵*\n*💠.hapo       ❴your text❵*\n*💠.babah       ❴your text❵*\n*💠.macn       ❴your text❵*\n*💠.nah       ❴your text❵*\n*╚═══❖══▣══▣══❖═══╝*\n\n*▷Template Credit: එ.මි.ස ¡v* '})
     }));
 }
 else if (Config.WORKTYPE == 'public') {
