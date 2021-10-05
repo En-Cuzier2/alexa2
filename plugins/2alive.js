2c49f61e37cf9736c48cf4306097ef933db8d865
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-10-05 20:22:19 +0530

    Update 2alive.js

--- plugins/2alive.js
+++ plugins/2alive.js
@@ -6,7 +6,7 @@ const Config = require('../config');
 
 if (Config.WORKTYPE == 'private') {
 	
-Asena.addCommand({pattern: 'mmpack', fromMe: true, desc: 'dose bot work / no'}, (async (message, match) => {
+Asena.addCommand({pattern: 'alive', fromMe: true, desc: 'dose bot work / no'}, (async (message, match) => {
 
 	        var r_text = new Array ();
 
@@ -23,7 +23,7 @@ await message.client.sendMessage(message.jid, Buffer.from(respoimage.data), Mess
 }
 else if (Config.WORKTYPE == 'public') {
 	
-	Asena.addCommand({pattern: 'mmpack', fromMe: false,}, (async (message, match) => {
+	Asena.addCommand({pattern: 'alive', fromMe: false,}, (async (message, match) => {
 
 	        var r_text = new Array ();
 
