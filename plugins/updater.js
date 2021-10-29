ef4f8bfd389697785d323e20b77dada5c5889517
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-10-30 00:15:08 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
+En cuzier 2021
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
@@ -32,7 +32,7 @@ Asena.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, de
         var degisiklikler = Lang.NEW_UPDATE;
         commits['all'].map(
             (commit) => {
-                degisiklikler += '🛠 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
+                degisiklikler += '🛠 *❲' + commit.date.substring(0, 10) + '❳*: ' + commit.message + ' <' + commit.author_name + '>\n';
             }
         );
         
